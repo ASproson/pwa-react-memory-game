65718b690747ab8148b9ba6930605fa759fa69b4
atlas <a.sproson@outlook.com>
2023-07-07 19:59:41 +0100

    Adds initial state and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import shuffle from "./utils/shuffle";
+import Card from "./components/Card";
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [cards, setCards] = useState(shuffle);
+
+  return (
+    <>
+      <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+          return (
+            <Card
+              key={id}
+              image={image}
+              // selected={selected}
+              onClick={() => {}}
+            />
+          );
+        })}
+      </div>
+      ;
+    </>
+  );
 }
 
 export default App;
