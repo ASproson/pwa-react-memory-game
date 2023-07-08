661e3dde8f907733c68eba3bbbcd53fa824661ff
atlas <a.sproson@outlook.com>
2023-07-08 09:41:57 +0100

    Updates manifest json

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ image, selected, onClick }) => {
         <img
           alt=""
           className="card-back"
-          src={"/assets/fireship.png"}
+          src={"/assets/code.png"}
           onClick={onClick}
         />
       </div>
