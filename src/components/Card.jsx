19eb6d26f2acb296610e6a2ae0d682410fafa499
atlas <a.sproson@outlook.com>
2023-07-08 09:21:03 +0100

    Updates props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,14 @@ const Card = ({ image, selected, onClick }) => {
   return (
     <div className="card">
       <div className={selected && "selected"}>
-        <img src={image} className="card-face" alt="" />
-        <img src={"/assets/fireship.png"} className="card-back" alt="" />
+        <img alt="" src={image} className="card-face" />
+
+        <img
+          alt=""
+          className="card-back"
+          src={"/assets/fireship.png"}
+          onClick={onClick}
+        />
       </div>
     </div>
   );
