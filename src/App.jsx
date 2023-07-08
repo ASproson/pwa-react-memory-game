70161dd0fce15b3cfded30edd9e92bebbdf26319
atlas <a.sproson@outlook.com>
2023-07-08 09:13:52 +0100

    Imports Header and adds New Game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import shuffle from "./utils/shuffle";
 import Card from "./components/Card";
 import { useState, useEffect } from "react";
+import Header from "./components/Header";
 
 function App() {
   const [cards, setCards] = useState(shuffle);
@@ -19,6 +20,12 @@ function App() {
     setPickOne(null);
     setPickTwo(null);
     setDisabled(false);
+  };
+
+  const handleNewGame = () => {
+    setWins(0);
+    handleTurn();
+    setCards(shuffle);
   };
 
   useEffect(() => {
@@ -61,6 +68,7 @@ function App() {
 
   return (
     <>
+      <Header handleNewGame={handleNewGame} wins={wins} />
       <div className="grid">
         {cards.map((card) => {
           const { image, id, matched } = card;
